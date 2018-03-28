ebcc35e97f6440d6eab30d22512a468a496d1537
Steven Choi <steven.m.choi@gmail.com>
2018-03-28 15:34:37 -0700

    Create Root component

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -7,6 +7,7 @@ import {
 import { fetchAllPokemon } from './util/api_util';
 import configureStore from './store/store';
 import { selectAllPokemon } from './reducers/selectors';
+import Root from './components/root';
 
 window.receiveAllPokemon = receiveAllPokemon;
 window.fetchAllPokemon = fetchAllPokemon;
@@ -19,6 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	window.dispatch = store.dispatch;
 	window.selectAllPokemon = selectAllPokemon;
 
-	const rootEl = document.getElementById('root');
-	ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+	const root = document.getElementById('root');
+	ReactDOM.render(<Root store={store} />, root);
 });
